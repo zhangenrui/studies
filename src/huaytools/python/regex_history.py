dc43813f5ca5f128b428b96612c5050b736ebeb5
imhuay <imhuay@163.com>
2022-03-03 21:06:54 +0800

    Update regex_history.py

--- src/huaytools/python/regex_history.py
+++ src/huaytools/python/regex_history.py
@@ -17,6 +17,13 @@ import doctest
 from typing import *
 from collections import defaultdict
 
+
+RE_MULTI_WHITE = re.compile(r'\s+')
+'''匹配多个空白符'''
+
+RE_MULTI_SPACE = re.compile(r' +')
+'''匹配多个空格'''
+
 RE_MULTI_LINE = re.compile(r'(\n\s*)+')
 '''匹配多个换行符'''
 
@@ -38,3 +45,6 @@ def _test():
 if __name__ == '__main__':
     """"""
     _test()
+
+    r = RE_CH.search('as 中s')
+    print(r)
